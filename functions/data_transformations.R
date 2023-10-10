e4fdc17bd63de90d5444b060fb8d66ba43c84e57
Sigurd Tverberg <sigurd.tverberg@student.nhh.no>
2023-10-10 15:56:30 +0200

    created transform_volume function

--- functions/data_transformations.R
+++ functions/data_transformations.R
@@ -10,9 +10,25 @@ transform_metadata_to_df <-
       unnest_wider(latLon)
   }
 
-
+# change date format to ISO8601 and adding offset
 to_iso8601 <-
   function(date_time, offset){
     iso8601(date_time+days(offset)) %>% 
       paste0("Z")
   }
+
+# transform volume information from json to data frame
+transform_volumes <- 
+  function(data){
+    data[[1]][[1]] %>% 
+      map(as_tibble) %>% 
+      list_rbind() %>% 
+      unnest_wider(edges) %>% 
+      unnest_wider(node) %>% 
+      unnest_wider(total) %>% 
+      unnest_wider(volumeNumbers) %>%
+      select(from, volume)
+  }
+  
+
+
